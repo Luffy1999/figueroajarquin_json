670fc76b8c43b702000d19054ad0b6deedb13dc4
Luffy1999 <85529503+Luffy1999@users.noreply.github.com>
2024-09-20 17:16:55 -0600

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,27 +1,26 @@
-<?
+<?php
 
 class conexion{
+
     public $host = '127.0.0.1';
-    public $db ='rrhh';
+    public $db = 'rrhh';
     public $user = 'root';
-    public $pass = ' ';
+    public $pass = '';
     public $port = '3306';
     public $charset = 'utf8mb4';
-    public $option = [
-        \PDO::ATTR_ERRMODE => \PDO::ATTR_ERRMODE_EXEPTION,
+    public $options = [
+        \PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-        \PDO::ATTR__EMULATE_PREPARES => false
+        \PDO::ATTR_EMULATE_PREPARES => false
     ];
 
     public function conectar(){
-        try{
-            $pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset={$this->charset};port={$this->port}",$this->user,$this->pass,$this->option);
-            return$pdo;
-        }catch(PDOExeption $exp) {
+        try {
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset={$this->charset};port={$this->port}",$this->user,$this->pass,$this->options);
+            return $pdo;
+        } catch (PDOException $exp) {
             echo("HUBO UN ERROR EN LA CONEXIÓN".$exp->getMessage());
         }
     }
 }
-
-
 ?>
